11f24812d84509c704dbb89c1afcf0341d08d9b9
Tom Rowbotham <tom.rowbotham@live.co.uk>
2024-12-12 14:06:48 +0000

    Add contact frontend

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './presentation/components/Layout';
 import { WalletContextProvider } from './contexts/WalletContext';
 import { routes } from './config/routes';
+import { Contact } from './presentation/pages/Contact';
 
 // Add Buffer polyfill
 import { Buffer } from 'buffer';
@@ -17,6 +18,7 @@ function App() {
             {routes.map((route) => (
               <Route key={route.path} {...route} />
             ))}
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </Layout>
       </WalletContextProvider>
